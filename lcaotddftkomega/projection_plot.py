138b435ee84ea45d51674b07bd97bbe94b1d0d88
Keenan Lyon <lyon.keenan@gmail.com>
2019-08-12 18:53:59 +0000

    Updated to include projections along a diagonal axis, useful for systems (like graphene) where the atoms align along a diagonal of the unit cell.

--- lcaotddftkomega/projection_plot.py
+++ lcaotddftkomega/projection_plot.py
@@ -103,7 +103,7 @@ def projection_data_y(cube_file, cube_projection, noproj = False, voxel = '0'):
 
     spacing_vec, values = read_cube_data(cube_file)
     idx=-1
-    length = int(spacing_vec[1][0])*float(spacing_vec[1][2])*0.5291772083
+    length = int(spacing_vec[1][0])*np.sqrt(float(spacing_vec[1][1])**2 + float(spacing_vec[1][2])**2)*0.5291772083
     # This yields the length of unit cell in Angstrom
 
     if not exists(cube_projection):
@@ -157,10 +157,8 @@ def projection_data_x(cube_file, cube_projection, noproj = False, voxel = '0'):
 
     spacing_vec, values = read_cube_data(cube_file)
     idx=-1
-    length = int(spacing_vec[0][0])*float(spacing_vec[0][1])*0.5291772083
+    length = int(spacing_vec[0][0])*np.sqrt(float(spacing_vec[0][1])**2 + float(spacing_vec[0][2])**2)*0.5291772083
     # This yields the length of unit cell in Angstrom
-    voxel = voxel
-    voxel = float(voxel)
 
     if not exists(cube_projection):
         data3D = np.zeros(shape=(int(spacing_vec[0][0]), int(spacing_vec[1][0]), int(spacing_vec[2][0])))
@@ -208,11 +206,68 @@ def projection_data_x(cube_file, cube_projection, noproj = False, voxel = '0'):
     return data, length
 
 
-def contour_maker(dtype = 'e', direction = 'z', cubedir='.', coarseness=1, brightness=0.75, emin=0.2, emax=20, ax=p, xenergy=False, noproj=False, voxel = '0'):
+def projection_data_diagonal(cube_file, cube_projection, diag_dir = 'nwse', angle = '90'):
+    # This method yields cube data projected along the "diagonal axis"
+    # This refers to z-axis data being summed out, and 
+
+    # diag_dir: 'nwse' goes from (N, 0) voxel to (0, N) voxel; 'nesw' goes from (0, 0) voxel to (N, N) voxel
+    # angle: angle between the two axes, useful for non-orthorhombic cells (degrees)
+
+    angle = np.pi/180.0*float(angle) # Change to radians
+
+    spacing_vec, values = read_cube_data(cube_file)
+    idx=-1
+    length1 = int(spacing_vec[0][0])*np.sqrt(float(spacing_vec[0][1])**2 + float(spacing_vec[0][2])**2)*0.5291772083
+    length2 = int(spacing_vec[1][0])*np.sqrt(float(spacing_vec[1][1])**2 + float(spacing_vec[1][2])**2)*0.5291772083
+    length = np.sqrt(length1**2 + length2**2 - 2*length1*length2*np.cos(angle))
+    # This yields the length of the bond-axis in Angstrom. Change the angle here depending on shape on unit cell
+
+    if not exists(cube_projection):
+        data3D = np.zeros(shape=(int(spacing_vec[0][0]), int(spacing_vec[1][0]), int(spacing_vec[2][0])))
+        data2D = np.zeros(shape=(int(spacing_vec[0][0]), int(spacing_vec[1][0])))
+        data = [] # x-axis projection plot
+
+        for i in range(0, int(spacing_vec[0][0])):
+            for j in range(0, int(spacing_vec[1][0])):
+                for k in range(0, int(spacing_vec[2][0])):
+                    idx+=1
+                    data3D[i, j, k]=values[idx]
+                data2D[i, j]=data3D[i, j, :].sum() # Sums out z, the outer column
+        Lx = int(spacing_vec[0][0])
+
+        for i in range(0, Lx+1):
+            if diag_dir == 'nwse':
+                rho_i=data2D[(i)%Lx,(Lx-i)%Lx] 
+                data.append([(i-Lx/2)*length/Lx, rho_i])
+            if diag_dir == 'nesw':
+                rho_i=data2D[(i)%Lx,(i)%Lx] 
+                data.append([(i-Lx/2)*length/Lx, rho_i])                
+         
+        data = sorted(data, key=lambda x: x[0])
+
+        # Prints the output to a .dat file so it can easily be read in for future contour plots
+        tmp=sys.stdout
+        sys.stdout = open(cube_projection, 'w')
+        for i in range(0, len(data)):
+            print(data[i][0], data[i][1])
+        sys.stdout.close()
+        sys.stdout=tmp
+        print("Printing file "+str(cube_projection))
+    else:
+        lines = open(cube_projection, 'r').readlines()
+        data = []
+        for line in lines:
+            data.append(np.fromstring(line, dtype=float, sep=' '))
+        data=np.array(data)
+
+    return data, length
+
+
+def contour_maker(dtype = 'e', direction = 'z', cubedir='.', coarseness=1, brightness=0.75, emin=0.2, emax=20, ax=p, xenergy=False, noproj=False, voxel = '0', diag_dir = 'nwse', angle = '90'):
     # This creates 1D projection plots along either the x, y, or z axes when given energy-resolved cube file data
 
     # type: 'e' or 'h', depending on the type of density you want plotted
-    # direction: 'x', 'y', or 'z', will project all cube directions onto the chosen axis
+    # direction: 'x', 'y', 'z', or 'diag' will project all cube directions onto the chosen axis
     # cubedir: the location of the cube files
     # coarseness: Choose the resolution of the energy; a higher coarseness will result in a larger energy spacing
     # brightness: Choose the brightness of the plot
@@ -222,6 +277,8 @@ def contour_maker(dtype = 'e', direction = 'z', cubedir='.', coarseness=1, brigh
     # xenergy: multiply the values of the contour plot by the energy to better resolve the spectra at higher energies
     # noproj: instead of doing a projection plot, choose to see a slice of 2D projected data at each energy
     # voxel: choose the voxel along the other direction at which the 'noproj' slice is taken. Use number_voxels() for help
+    # diag_dir: 'nwse' goes from (N, 0) voxel to (0, N) voxel; 'nesw' goes from (0, 0) voxel to (N, N) voxel
+    # angle: angle between the two axes, useful for non-orthorhombic cells when using projection_data_diagonal (degrees)
 
     fnames = os.listdir(cubedir)
     codedir = os.getcwd()
@@ -241,18 +298,24 @@ def contour_maker(dtype = 'e', direction = 'z', cubedir='.', coarseness=1, brigh
             energy = re.findall(r'[-+]?(\d*\.\d*)', f)[0]
             if np.ceil(float(energy)*100)%int(coarseness) == 0: 
                 energies.append(energy)
+                # The z-axis method only does summation, so noproj = False for these calculations
                 if direction == 'z':
                     data, length = projection_data_z(f, 'z_{}eV_'.format(energy)+dtype+'.dat')
+                # The y-axis method can do 1D projection or 2D slices, depending on the value of noproj
                 if direction == 'y':
                     if noproj:
                         data, length = projection_data_y(f, 'y_{}eV_'.format(energy)+'voxel_'+str(voxel)+'_'+dtype+'.dat', noproj, voxel)
                     else:
                         data, length = projection_data_y(f, 'y_{}eV_'.format(energy)+dtype+'.dat')
+                # The x-axis method can do 1D projection or 2D slices, depending on the value of noproj
                 if direction == 'x':
                     if noproj:
                         data, length = projection_data_x(f, 'x_{}eV_'.format(energy)+'voxel_'+str(voxel)+'_'+dtype+'.dat', noproj, voxel)
                     else:
                         data, length = projection_data_x(f, 'x_{}eV_'.format(energy)+dtype+'.dat')
+                # The bond method does not do a summation, so noproj = True for these calculations
+                if direction == 'diag':
+                    data, length = projection_data_diagonal(f, 'diag_{}eV_'.format(energy)+dtype+'.dat', diag_dir, angle)
                 axis = []
                 density = []
                 for n in range(0, len(data)):
@@ -310,8 +373,6 @@ def contour_maker(dtype = 'e', direction = 'z', cubedir='.', coarseness=1, brigh
     ax.plot([zmin, zmax], [639, 639], linestyle='-.', color='w', linewidth=3.)
     ax.plot([zmin, zmax], [382, 382], linestyle='-.', color='w', linewidth=3.)
     ax.plot([zmin, zmax], [319, 319], linestyle='-.', color='w', linewidth=3.)
-    ax.ylim(zmin,zmax)
-
     ax.axis((float(emin), float(emax), zmin, zmax))
 
     ax.show()
